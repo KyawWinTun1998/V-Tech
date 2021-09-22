1957657169f6b4ef2a2f787b560dc619cf4fe554
KyawWinTun-vix <vixenhello1998@gmail.com>
2021-09-22 15:31:16 +0630

    home and course

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = ()=>{
+menu.onclick = () =>{
       menu.classList.toggle('fa-times');
       navbar.classList.toggle('active');
 }
 
 window.onscroll = () =>{
-      menu.classList.toggle('fa-times');
-      navbar.classList.toggle('active');
+      menu.classList.remove('fa-times');
+      navbar.classList.remove('active');
 }
 
 
